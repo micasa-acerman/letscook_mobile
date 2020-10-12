be4473f3762bce9bbfba21f05165102faca02e9e
Micasa Acerman <koha009@yandex.ru>
2020-10-12 14:48:40 +0300

    components: create recipe component

---
+++ components/PostComponent.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Image, StyleSheet, Text, View } from 'react-native'
+import Post from '../models/Post'
+
+export default function PostComponent({post}:{post:Post}) {
+    const media = post['_embedded']['wp:featuredmedia']
+    // console.log(media)
+    
+    const imageSrc:string = media?media[0].media_details.sizes.medium_large.source_url:""
+    const title:string = post.title.rendered
+    return (
+      <View style={styles.inner}>
+        <Image style={styles.image} source={{ uri: imageSrc }} />
+        <Text style={styles.title} numberOfLines={3}>
+          {title}
+        </Text>
+      </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inner:{
+        width: 164,
+        marginRight: 10
+    },
+    image:{
+        width: 164,
+        height: 164
+    },
+    title:{
+        color: '#676767',
+        fontFamily: 'aqua',
+        fontSize: 16,
+    },
+})
