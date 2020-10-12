7fe35384e02a6802ee0d897d960e6bf69d53f57c
Micasa Acerman <koha009@yandex.ru>
2020-10-12 14:47:39 +0300

    api: append chat endpoints

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,18 @@
 import AsyncStorage from "@react-native-community/async-storage"
 import Axios from "axios"
-import { GET_MY_INFO, GET_POSTS_URL, SIGN_IN_URL } from "../constants/Common"
+import { GET_DIALOGS, GET_MY_INFO, GET_POSTS_URL, SIGN_IN_URL } from "../constants/Common"
 import AuthData from "../models/AuthData"
+import Dialog from "../models/Dialog"
+import Message from "../models/Message"
+import Post from "../models/Post"
 import User from "../models/User"
 
-export class REST{
+type BoxType = {
+  SENTBOX: "sentbox";
+  INBOX:"inbox",
+  STARRED:'starred'
+};
+export default class REST{
     static signIn (username:string,password:string):Promise<AuthData> {
         return Axios
         .request<AuthData>({
@@ -19,9 +27,10 @@ export class REST{
           return response.data
         })
     }
-    static getPosts ():Promise<Array<AuthData>> {
-      return Axios.request<Array<AuthData>>({
-        url: GET_POSTS_URL,
+    static getPosts (authorId?:string):Promise<Array<Post>> {
+      const url = `${GET_POSTS_URL}${authorId?`&author=${authorId!}`:''}`
+      return Axios.request<Array<Post>>({
+        url: url,
         method: "GET",
       }).then((response) => {
         return response.data;
@@ -29,19 +38,59 @@ export class REST{
     }
     static async getMyInfo():Promise<User> {
       const token = await REST.getToken();
+      console.log('Token',token);
+      
       return Axios.request<User>({
         url: GET_MY_INFO,
         method: "GET",
         headers: {
           'Authorization': `Bearer ${token}`
         }
-      }).then((response) => {
-        console.log(response);
-        
+      }).then((response) => {        
         return response.data;
       });
     }
-  
+    
+
+    static async getDialogs():Promise<Array<Dialog>> {
+      const token = await REST.getToken();
+      const response = await Axios.request<Array<Dialog>>({
+        url: GET_DIALOGS,
+        method: "GET",
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      return response.data;
+    }
+    
+    static async getMessages(user_id:number):Promise<Array<Message>> {
+      const token = await REST.getToken();
+      const response = await Axios.request<Array<Message>>({
+        url: `${GET_DIALOGS}/${user_id}`,
+        method: "GET",
+        headers: {
+          'Authorization': `Bearer ${token}`
+        }
+      })
+      
+      return response.data;
+    }
+    static async sendMessage(user_id:number,message:string) {
+      const token = await REST.getToken();
+      const response = await Axios.request({
+        url: GET_DIALOGS,
+        method: "POST",
+        headers: {
+          'Authorization': `Bearer ${token}`
+        },
+        data:{
+          to: user_id,
+          message
+        }
+      })
+      return response.data;
+    }
 
     private static getToken():Promise<string|null>{
       return AsyncStorage.getItem('token')
--- constants/Common.ts
+++ constants/Common.ts
@@ -1,5 +1,8 @@
 
-export const DOMAIN = "http://kafka-webpage:8080"
+export const DOMAIN = "http://192.168.42.84:8080"
 export const SIGN_IN_URL = DOMAIN+"/wp-json/jwt-auth/v1/token"
 export const GET_POSTS_URL = DOMAIN+"/wp-json/wp/v2/posts?_embed"
 export const GET_MEDIA_URL = DOMAIN+"/wp-json/wp/v2/media/"
+export const GET_MY_INFO = DOMAIN+"/wp-json/wp/v2/users/me?context=edit"
+export const GET_DIALOGS = DOMAIN+"/wp-json/conversation/v1/dialogs"
+export const HOME_ITEMS_PER_PAGE = 2
