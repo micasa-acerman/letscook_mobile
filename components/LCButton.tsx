45c99247f7a885877fbf27dfc36d2cb50d359402
Micasa Acerman <koha009@yandex.ru>
2020-10-13 22:15:38 +0300

    components(fix): define secondary button style

--- components/LCButton.tsx
+++ components/LCButton.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {  Button, StyleSheet,Text, TextStyle, ViewStyle } from 'react-native'
-import { TouchableHighlight } from 'react-native-gesture-handler'
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 export enum ButtonStyle {
     Primary,
@@ -13,43 +13,65 @@ interface ButtonProps {
     type?:ButtonStyle,
     containerStyle?:ViewStyle,
     textStyle?:TextStyle,
-    onClick?:Function
+    onClick?:()=>void
 }
-export default function LCButton({children="Button",type=ButtonStyle.Primary,textStyle,containerStyle,onClick=Function()}:ButtonProps) {
-    switch(type){
-        case ButtonStyle.Primary:
-            return (
-                <TouchableHighlight underlayColor="#F47E7E" activeOpacity={0.8} style={[styles.Primary,containerStyle]} onPress={onClick}>
-                    <Text style={[styles.text,textStyle]}>{children}</Text>
-                </TouchableHighlight>
-            )
-        case ButtonStyle.Secondary:
-            return (
-                <TouchableHighlight underlayColor="transparent" activeOpacity={0.8} style={[styles.Secondary,containerStyle]} onPress={onClick}>
-                    <Text style={[styles.text,textStyle]}>{children}</Text>
-                </TouchableHighlight>
-            )
-        case ButtonStyle.Link:
-            return (
-                <TouchableHighlight underlayColor="transparent" activeOpacity={0.8} style={[styles.Link,containerStyle]} onPress={onClick}>
-                    <Text style={[styles.text,textStyle]}>{children}</Text>
-                </TouchableHighlight>
-            )
+
+export default function LCButton({children="Button",type=ButtonStyle.Primary,textStyle,containerStyle,onClick=()=>{}}:ButtonProps) {
+    switch (type) {
+      case ButtonStyle.Primary:
+        return (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={[styles.containerPrimary, containerStyle]}
+            onPress={onClick}
+          >
+            <Text style={[styles.text, textStyle]}>{children}</Text>
+          </TouchableOpacity>
+        );
+      case ButtonStyle.Secondary:
+        return (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={[styles.containerSecondary, containerStyle]}
+            onPress={onClick}
+          >
+            <Text style={[styles.text, styles.textSecondary, textStyle]}>
+              {children}
+            </Text>
+          </TouchableOpacity>
+        );
+      case ButtonStyle.Link:
+        return (
+          <TouchableOpacity
+            activeOpacity={0.8}
+            style={[styles.containerLink, containerStyle]}
+            onPress={onClick}
+          >
+            <Text style={[styles.text, textStyle]}>{children}</Text>
+          </TouchableOpacity>
+        );
     }
 }
 
 const styles = StyleSheet.create({
-    Primary:{
+    containerPrimary:{
         backgroundColor: '#EB5757',
         height:40,
         flexDirection: 'row',
         justifyContent: 'center',
         alignItems: 'center'
     },
-    Secondary:{
-        backgroundColor: "transparent"
+    containerSecondary:{
+        backgroundColor: "transparent",
+        borderRadius: 20,
+        borderWidth: 1,
+        height:40,
+        borderColor: '#EB5757',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
-    Link:{
+    containerLink:{
         fontSize: 16,
         color: '#EAEAEA',
         alignItems: 'center'
@@ -58,5 +80,8 @@ const styles = StyleSheet.create({
         color: '#ffffff',
         fontSize: 18,
         fontFamily: 'aqua'
+    },
+    textSecondary:{
+        color: '#EB5757'
     }
 })
