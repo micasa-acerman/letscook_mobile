ce219700665af43778702e15c94303b57ff30059
Micasa Acerman <koha009@yandex.ru>
2020-10-14 16:47:59 +0300

    rest: add category endpoint

--- constants/Common.ts
+++ constants/Common.ts
@@ -5,4 +5,5 @@ export const GET_POSTS_URL = DOMAIN+"/wp-json/wp/v2/posts?_embed"
 export const GET_MEDIA_URL = DOMAIN+"/wp-json/wp/v2/media/"
 export const GET_MY_INFO = DOMAIN+"/wp-json/wp/v2/users/me?context=edit"
 export const GET_DIALOGS = DOMAIN+"/wp-json/conversation/v1/dialogs"
+export const GET_CATEGORIES = DOMAIN+"/wp-json/wp/v2/categories"
 export const HOME_ITEMS_PER_PAGE = 2
--- models/Category.ts
+++ models/Category.ts
@@ -1,7 +1,33 @@
-export default interface Category {
-    "id": number,
-    "link":string,
-    "name":string,
-    "slug":string,
-    "taxonomy":string,
+export default interface Category{
+  id: number,
+  count: number,
+  description: string,
+  link: string,
+  name: string,
+  slug: string,
+  taxonomy: string,
+  parent: number,
+  category_image?: {
+      id:number,
+      title: string,
+      filename:string,
+      filesize: number,
+      url:string,
+      link:string,
+      alt:string,
+      author:string
+      date:string,
+      modified:string,
+      type:string,
+      mime_type:string,
+      icon:string,
+      width:number,
+      height:number,
+      sizes:{
+          thumbnail:string,
+          medium:string,
+          medium_large:string,
+          large:string
+      }
   }
+}
