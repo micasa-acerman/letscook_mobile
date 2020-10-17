cd1509370683f8672c504a6fb4e033c68d4000fc
Micasa Acerman <koha009@yandex.ru>
2020-10-17 21:05:31 +0300

    config: prepare release

--- constants/Common.ts
+++ constants/Common.ts
@@ -1,6 +1,6 @@
 import { Platform } from "react-native";
 
-export const DOMAIN = "http://192.168.42.158:8080";
+export const DOMAIN = "https://letscook15.000webhostapp.com";
 export const SIGN_IN_URL = DOMAIN + "/wp-json/jwt-auth/v1/token";
 export const SIGN_UP_URL = DOMAIN + "/wp-json/wp/v2/users/register";
 export const GET_POSTS_URL =
@@ -15,6 +15,9 @@ export const GET_USERS = DOMAIN + "/wp-json/wp/v2/users";
 export const CHANGE_PASSWORD = DOMAIN + "/wp-json/conversation/v1/password"
 import * as ImagePicker from "expo-image-picker";
 export const HOME_ITEMS_PER_PAGE = 2;
+
+
+export const ALLOW_MEDIA_EXTENSIONS = ['.png','.jpg','.jpeg']
 
 export const grantPermissions = async () => {
   if (Platform.OS != "web") {
